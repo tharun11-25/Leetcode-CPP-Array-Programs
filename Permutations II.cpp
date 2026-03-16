b9919e4a73d3b34a2d489207f5995439b85cd89a
Tharun V S <v.s.tharun0709@gmail.com>
2026-03-16 11:00:24 +0530

    Create Permutations II.cpp

---
+++ Permutations II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+      void backtrack(vector<int>& nums, vector<int>& temp, vector<vector<int>>& result, vector<bool>& used) {
+        if (temp.size() == nums.size()) {
+            result.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (used[i]) continue;
+
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+
+            used[i] = true;
+            temp.push_back(nums[i]);
+
+            backtrack(nums, temp, result, used);
+
+            temp.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+           vector<vector<int>> result;
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+
+        sort(nums.begin(), nums.end()); // important
+
+        backtrack(nums, temp, result, used);
+        return result;
+    }
+};
